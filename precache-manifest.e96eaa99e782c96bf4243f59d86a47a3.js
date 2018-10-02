c122d5768bd2a88c8e22b013adaac99e6e021e1b
L Lee <lauralee.sh@gmail.com>
2018-10-02 10:36:10 -0700

    Updates

---
+++ precache-manifest.e96eaa99e782c96bf4243f59d86a47a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/NY-Times-Scrubber/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "683a4407683deb5ec48f",
+    "url": "/NY-Times-Scrubber/static/js/runtime~main.683a4407.js"
+  },
+  {
+    "revision": "38cbf1b920adf453cb55",
+    "url": "/NY-Times-Scrubber/static/js/main.38cbf1b9.chunk.js"
+  },
+  {
+    "revision": "7089717f1da19893d0c8",
+    "url": "/NY-Times-Scrubber/static/js/1.7089717f.chunk.js"
+  },
+  {
+    "revision": "38cbf1b920adf453cb55",
+    "url": "/NY-Times-Scrubber/static/css/main.7a7bb542.chunk.css"
+  },
+  {
+    "revision": "5495c1008c4c17d485bb7a08c5fcf2c4",
+    "url": "/NY-Times-Scrubber/index.html"
+  }
+];
